146a56b32788d2688a51f66bc80c44ddd6f21006
OLGON92 <ogon318@gmail.com>
2023-05-19 13:55:51 -0700

    add a nav bar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,58 @@
+import { Button, NavbarBrand } from "react-bootstrap";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const NavBar = () => {
+  const { auth, signOut } = useAuth();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    try {
+      const {error} = await signOut();
+      console.log(error);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>Already Copped</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {!auth && (
+              <Nav.Link as={Link} to="/signIn">
+                Sign In
+              </Nav.Link>
+            )}
+            {!auth && (
+              <Nav.Link as={Link} to="/signUp">
+                Sign Up
+              </Nav.Link>
+            )}
+            {auth && (
+              <Nav.Link as={Link} to="/"> 
+                Dashboard
+              </Nav.Link>
+            )}
+          </Nav>
+          <Nav>
+            {auth && (
+              <Nav.Link as={Button} to="/" onClick={handleLogout}>
+                Sign Out
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
