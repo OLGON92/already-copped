7a3615aecd6b44ede68936c431c524f12d47a5b3
OLGON92 <ogon318@gmail.com>
2023-05-19 02:16:11 -0700

    implement login functionality

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,15 +1,34 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { supabase } from "../supabase";
 
 //Maybe add React.createContext if this doesnt work
 const AuthContext = createContext({});
 
 export const useAuth = () => useContext(AuthContext);
 
+const signIn = (email, password) =>
+  supabase.auth.signInWithPassword({ email, password });
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_IN") {
+        setUser(session.user);
+        setAuth(true);
+      }
+    });
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signIn }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
