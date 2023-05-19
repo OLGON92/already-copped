95f5dde8c5a83218a122efa664bacd82d33f844b
OLGON92 <ogon318@gmail.com>
2023-05-19 02:46:20 -0700

    finish refactor of signIn component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,30 +1,71 @@
 import React, { useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useAuth } from "../context/AuthProvider";
+import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Card, Form } from "react-bootstrap";
 
-export function SignIn() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
+const SignIn = () => {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
 
-  async function handleSubmit(event) {
-    event.preventDefault()
-
-    
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setErrorMsg("");
+      if (!passwordRef.current?.value || !emailRef.current?.value) {
+        return setErrorMsg("Please fill all the fields!");
+      }
+      const {
+        data: { user, session },
+        error
+      } = await signIn(emailRef.current.value, passwordRef.current.value);
+      if (error) setErrorMsg(error.message);
+      if (user && session) navigate("/");
+    } catch (error) {
+      setErrorMsg("Email or password incorrect");
+    }
+    setLoading(false);
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="input-email">Email</label>
-        <input type="email" id="input-email" ref={emailRef} required />
-
-        <label htmlFor="input-password">Password</label>
-        <input type="password" id="input-password" ref={passwordRef} required />
-        <br />
-        <button type="submit">Sign In</button>
-      </form>
-      <p>
-        Don't have an account? <Link to="/signUp">Sign Up</Link>
-      </p>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign In</h2>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control ref={emailRef} type="email" required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control ref={passwordRef} type="password" required />
+            </Form.Group>
+            {errorMsg && (
+              <Alert 
+                variant="danger"
+                onClose={() => setErrorMsg("")}
+                dismissible>
+                {errorMsg}
+              </Alert>
+            )}
+            <div className="text-center mt-2">
+              <Button disabled={loading} type="submit" className="w-50">
+                Sign In
+              </Button>
+            </div>
+          </Form>
+        </Card.Body>
+        <div className="w-100 text-center mt-2">
+          Need an account? <Link to="/signUp">Sign Up</Link>
+        </div>
+      </Card>
     </>
-  )
-}
+  );
+};
+
+export default SignIn;
