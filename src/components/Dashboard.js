72a27568e2c88205c70cbdb1edd9fb7abdb854ba
OLGON92 <ogon318@gmail.com>
2023-05-18 18:44:49 -0700

    add a function and a sign out button

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Dashboard() {
+  async function handleSignOut() {
+
+  }
+
+  return (
+    <div>
+      <p>Welcome to Already Copped!</p>
+      <button onClick={handleSignOut}>Sign Out</button>
+    </div>
+  )
+}
